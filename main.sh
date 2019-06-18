eb9453c94e19b312e7bbf65d3d5e2536084fae6f
Yad Smood <y.s.inside@gmail.com>
2019-06-18 20:48:23 +0900

    init

---
+++ main.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env sh
+
+install() {
+    echo "install into $os"
+    echo downloading: $url
+    remove $bin-$suffix
+    $download $url
+
+    extract $bin-$suffix
+    remove $bin-$suffix
+    move $bin /usr/local/bin/
+    echo installed to /usr/local/bin/$bin
+}
+
+remove() {
+    if [ -w $(dirname $1) ]; then
+        rm $1
+    else
+        sudo rm $1
+    fi
+}
+
+move() {
+    if [ -w $(dirname $2) ]; then
+        ln $1 $2 
+    else
+        sudo ln $1 $2
+    fi
+
+    remove $1
+}
+
+extract() {
+    if [ ! -w $(dirname $1) ]; then
+        extractor="sudo $extractor"
+    fi
+
+    $extractor $1
+}
+
+select_url() {
+    if [ -z "$ver" ]
+    then
+        url=`$get https://api.github.com/repos/$repo/releases/latest | grep $bin-$os | grep /releases/download/ | cut -d '"' -f 4`
+    else
+        url="https://github.com/$repo/releases/download/$ver/$bin-$suffix"
+    fi
+}
+
+select_downloader() {
+    if which wget > /dev/null; then
+        get="wget -O-"
+        download="wget"
+    else
+        get="curl -L"
+        download="curl -JOL"
+    fi
+
+    # if cwd is not writable prepend sudo
+    if [ ! -w `pwd` ]; then
+        download="sudo $download"
+    fi
+}
+
+select_os() {
+    case $(uname) in
+    Darwin)
+        os="mac"
+        suffix="$os.zip"
+        extractor=unzip
+        ;;
+    Linux)
+        os="linux"
+        suffix="$os.tar.gz"
+        extractor="tar -xzf"
+        ;;
+    *)
+        echo "os not supported..."
+        exit 1
+        ;;
+    esac
+}
+
+select_downloader
+select_os
+select_url
+install
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Github Install
+
+A script to help download and install github executable release.
+
+## Example
+
+Install the latest:
+
+```bash
+curl -L https://git.io/fjaxx | repo=ysmood/gokit bin=godev sh
+```
+
+Or
+
+```bash
+wget -O- https://git.io/fjaxx | repo=ysmood/gokit bin=godev sh
+```
+
+Install a specific version:
+
+```bash
+curl -L https://git.io/fjaxx | repo=ysmood/gokit bin=godev sh
+```
+
+Or
+
+```bash
+wget -O- https://git.io/fjaxx | repo=ysmood/gokit bin=godev ver=v0.10.3 sh
+```
